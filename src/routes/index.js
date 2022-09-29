683242a6d35fcdd76feb3756f40451c9af3f8250
Amarendra Varma <76420108+amarvarma7336@users.noreply.github.com>
2022-09-29 16:13:00 +0530

    Add files via upload

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,1 +1,19 @@
-route
+const router = require("express").Router();
+const registerUserController = require("../controllers/registerUser");
+const loginController = require("../controllers/loginController");
+const uploadFileController = require("../controllers/uploadFileController");
+
+// login route
+
+router.post("/login", loginController.userLogin);
+
+// create user
+
+router.post("/create-user", registerUserController.createUser);
+
+// upload json file
+
+router.post("/upload-json-file", uploadFileController.uploadJsonFile);
+router.get("/get-json-data", uploadFileController.getAllJsonData);
+
+module.exports = router;
